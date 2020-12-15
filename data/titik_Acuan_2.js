1a9a1b893c8c75eb4051d4edf9a61cd011a42859
IsabellaV01 <76053149+IsabellaV01@users.noreply.github.com>
2020-12-15 21:00:46 +0700

    Add files via upload

---
+++ data/titik_Acuan_2.js
@@ -0,0 +1,8 @@
+var json_titik_Acuan_2 = {
+"type": "FeatureCollection",
+"name": "titik_Acuan_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.413841534491922, -7.151789999248077 ] } }
+]
+}
